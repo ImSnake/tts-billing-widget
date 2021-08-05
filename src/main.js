d9f16b014905c1038af3fa4791b62a8ec3db37c4
Yuliya Pulikova <pulikova.j@gmail.com>
2021-08-05 15:50:45 +0300

    Добавлен класс на головной конейнер

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+document.getElementById('vue-app-billing-widget').classList.add('elzTheme-light');
 createApp(App).mount("#vue-app-billing-widget");
