559ded83d48d33972a996101f4be3ba6c05cb5ee
Yuliya Pulikova <pulikova.j@gmail.com>
2022-10-17 15:21:46 +0300

    коммит от 17.10.2022

--- src/components/Sessions.vue
+++ src/components/Sessions.vue
@@ -69,16 +69,64 @@ export default {
           <thead class="elz tbody pad p8 pV10 p-sticky p-T z1 bor borB2 br br-secondary bg bg-secondary fn fn-secondary-t brL-5 brFD brLF20">
           <tr class="tr">
             <td class="td">
-              <div class="elz d-block bold">Внешний идентификатор</div>
+              <div class="elz d-block bold">User-Name</div>
             </td>
             <td class="td">
-              <div class="elz d-block bold">Тип сессии</div>
+              <div class="elz d-block bold">User-Password</div>
             </td>
             <td class="td">
               <div class="elz d-block bold">Начата</div>
             </td>
             <td class="td">
               <div class="elz d-block bold">Завершена</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block bold">Последние обновления</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block bold">Причина завершения</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block bold">Speed-Download-Bps</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block bold">Speed-Upload-Bps</div>
+            </td>
+
+            <!--<td class="td">
+              <div class="elz d-block bold">Входящий трафик за сессию</div>
+            </td>-->
+
+            <!--<td class="td">
+              <div class="elz d-block bold">Исходящий трафик за сессию</div>
+            </td>-->
+
+            <td class="td">
+              <div class="elz d-block bold">Внешний идентификатор</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block bold">Calling-Station-Id</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block bold">Framed-Ip-Address</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block bold">NAS-Identifier</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block bold">NAS-Port-Id</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block bold">Service-State</div>
+            </td>
+
+
+
+
+
+
+<!--            <td class="td">
+              <div class="elz d-block bold">Тип сессии</div>
             </td>
             <td class="td">
               <div class="elz d-block bold">Состояние</div>
@@ -89,17 +137,8 @@ export default {
             <td class="td">
               <div class="elz d-block bold">Операторское оборудование</div>
             </td>
-            <!--<td class="td">
-              <div class="elz d-block bold">Абонентский профиль</div>
-            </td>-->
             <td class="td">
               <div class="elz d-block bold">Операторский профиль</div>
-            </td>
-            <!--<td class="td">
-              <div class="elz d-block bold">Сервисный абонентский профиль</div>
-            </td>-->
-            <td class="td">
-              <div class="elz d-block bold">Последнее обновления</div>
             </td>
             <td class="td">
               <div class="elz d-block bold">Последняя загрузка</div>
@@ -111,34 +150,10 @@ export default {
               <div class="elz d-block bold">Идентификатор сеанса загрузки</div>
             </td>
             <td class="td">
-              <div class="elz d-block bold">Причина завершения</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block bold">User-Name</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block bold">User-Password</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block bold">Speed-Download-Bps</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block bold">Speed-Upload-Bps</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block bold">Service-State</div>
-            </td>
-            <td class="td">
               <div class="elz d-block bold">Auth-Scheme</div>
             </td>
             <td class="td">
               <div class="elz d-block bold">Acct-Multi-Session-Id</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block bold">Calling-Station-Id</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block bold">Framed-Ip-Address</div>
             </td>
             <td class="td">
               <div class="elz d-block bold">Frames-User</div>
@@ -147,35 +162,73 @@ export default {
               <div class="elz d-block bold">NAS-IP-Address</div>
             </td>
             <td class="td">
-              <div class="elz d-block bold">NAS-Identifier</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block bold">NAS-Port-Id</div>
-            </td>
-            <td class="td">
               <div class="elz d-block bold">NAS-Type</div>
             </td>
             <td class="td">
               <div class="elz d-block bold">Packet-Src-IP-Address</div>
-            </td>
-            <!--<td class="td">
-              <div class="elz d-block bold">User-Name</div>
             </td>-->
           </tr>
           </thead>
           <tbody class="elz tbody pad p8 stripOdd stripHover">
           <tr v-for="(sessionItem, index) in sessionsData" class="tr">
             <td class="td">
-              <div class="elz d-block">{{ sessionItem.externalIDString }}</div>
+              <div class="elz d-block">{{ sessionItem.sessionUsername }}</div>
             </td>
             <td class="td">
-              <div class="elz d-block">{{ sessionItem.sessionTypeString }}</div>
+              <div class="elz d-block">{{ sessionItem.sessionPassword }}</div>
             </td>
             <td class="td">
               <div class="elz d-block">{{ sessionItem.sessionStartDateTime }}</div>
             </td>
             <td class="td">
               <div class="elz d-block">{{ sessionItem.sessionFinishDateTime }}</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block">{{ sessionItem.sessionLastUpdateDateTime }}</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block">{{ sessionItem.terminationReasonName }}</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block">{{ sessionItem.speedDownload }}</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block">{{ sessionItem.speedUpload }}</div>
+            </td>
+
+            <!--<td class="td">
+              <div class="elz d-block bold">Входящий трафик за сессию (будет добавлено)</div>
+            </td>-->
+
+            <!--<td class="td">
+              <div class="elz d-block bold">Исходящий трафик за сессию (будет добавлено)</div>
+            </td>-->
+
+            <td class="td">
+              <div class="elz d-block">{{ sessionItem.externalIDString }}</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block">{{ sessionItem.callingStationIdString }}</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block">{{ sessionItem.framedIPAddressString }}</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block">{{ sessionItem.nasIdString }}</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block">{{ sessionItem.nasPortIdString }}</div>
+            </td>
+            <td class="td">
+              <div class="elz d-block">{{ sessionItem.serviceState }}</div>
+            </td>
+
+
+
+
+
+<!--            <td class="td">
+              <div class="elz d-block">{{ sessionItem.sessionTypeString }}</div>
             </td>
             <td class="td">
               <div class="elz d-block">{{ sessionItem.sessionStatusName }}</div>
@@ -186,17 +239,8 @@ export default {
             <td class="td">
               <div class="elz d-block">{{ sessionItem.providerEquipmentName }}</div>
             </td>
-            <!--<td class="td">
-              <div class="elz d-block">???</div>
-            </td>-->
             <td class="td">
               <div class="elz d-block">{{ sessionItem.providerProfileId }}</div>
-            </td>
-            <!--<td class="td">
-              <div class="elz d-block">???</div>
-            </td>-->
-            <td class="td">
-              <div class="elz d-block">{{ sessionItem.sessionLastUpdateDateTime }}</div>
             </td>
             <td class="td">
               <div class="elz d-block">{{ sessionItem.sessionLastLoadDateTime }}</div>
@@ -208,34 +252,10 @@ export default {
               <div class="elz d-block">{{ sessionItem.loadSeanceIdString }}</div>
             </td>
             <td class="td">
-              <div class="elz d-block">{{ sessionItem.terminationReasonName }}</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block">{{ sessionItem.sessionUsername }}</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block">{{ sessionItem.sessionPassword }}</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block">{{ sessionItem.speedDownload }}</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block">{{ sessionItem.speedUpload }}</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block">{{ sessionItem.serviceState }}</div>
-            </td>
-            <td class="td">
               <div class="elz d-block">{{ sessionItem.authScheme }}</div>
             </td>
             <td class="td">
               <div class="elz d-block">{{ sessionItem.multiSessionIdString }}</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block">{{ sessionItem.callingStationIdString }}</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block">{{ sessionItem.framedIPAddressString }}</div>
             </td>
             <td class="td">
               <div class="elz d-block">{{ sessionItem.framedUser }}</div>
@@ -244,19 +264,10 @@ export default {
               <div class="elz d-block">{{ sessionItem.nasIPAddressString }}</div>
             </td>
             <td class="td">
-              <div class="elz d-block">{{ sessionItem.nasIdString }}</div>
-            </td>
-            <td class="td">
-              <div class="elz d-block">{{ sessionItem.nasPortIdString }}</div>
-            </td>
-            <td class="td">
               <div class="elz d-block">{{ sessionItem.sessionNasTypeString }}</div>
             </td>
             <td class="td">
               <div class="elz d-block">{{ sessionItem.packetSrcIPAddrString }}</div>
-            </td>
-            <!--<td class="td">
-              <div class="elz d-block">{{ sessionItem.userName }}</div>
             </td>-->
           </tr>
           </tbody>
